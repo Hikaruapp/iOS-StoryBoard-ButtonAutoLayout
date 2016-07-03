596ded8bc64f1ed746d79ec4f0e0bd41720825a1
Hikaruapp <info-iphone@hikaruapp.jpn.com>
2016-07-03 12:45:18 +0900

    first commit

---
+++ AutoLayout001/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayout001
+//
+//  Created by Yanase Yuji on 2016/06/27.
+//  Copyright © 2016年 hikaruApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoLayout001/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AutoLayout001
+//
+//  Created by Yanase Yuji on 2016/06/27.
+//  Copyright © 2016年 hikaruApp. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
